3796808ac084a9ac1acdf92e4fc801a6c95fb4c1
softnet <daniel@softnet.cloud>
2026-02-26 17:59:18 -0500

    fixes

--- global/iam.tf
+++ global/iam.tf
@@ -31,25 +31,190 @@ resource "aws_iam_role" "github_actions" {
   tags = merge({ ManagedBy = "Terraform" }, var.tags)
 }
 
-resource "aws_iam_role_policy" "github_actions_ecr" {
-  name = "ibank-github-actions-ecr-policy"
+resource "aws_iam_role_policy" "github_actions_permissions" {
+  name = "ibank-github-actions-policy"
   role = aws_iam_role.github_actions.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "ecr:PutImage",
-        "ecr:InitiateLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:CompleteLayerUpload"
-      ]
-      Resource = "*"
-    }]
+    Statement = [
+
+      ############################################
+      # S3 — read and write Terraform state files
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::ibank-terraform-state",
+          "arn:aws:s3:::ibank-terraform-state/*"
+        ]
+      },
+
+      ############################################
+      # DynamoDB — state locking
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:DescribeTable"
+        ]
+        Resource = "arn:aws:dynamodb:us-east-1:121897425968:table/ibank-terraform-locks"
+      },
+
+      ############################################
+      # KMS — decrypt and encrypt state files
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:GenerateDataKey",
+          "kms:DescribeKey"
+        ]
+        Resource = "arn:aws:kms:us-east-1:121897425968:alias/ibank-terraform-state"
+      },
+
+      ############################################
+      # ECR — push and pull container images
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:CreateRepository",
+          "ecr:DescribeRepositories",
+          "ecr:ListTagsForResource",
+          "ecr:TagResource",
+          "ecr:PutLifecyclePolicy",
+          "ecr:GetLifecyclePolicy"
+        ]
+        Resource = "*"
+      },
+
+      ############################################
+      # IAM — manage roles and policies
+      # Needed because Terraform creates IAM roles
+      # for EKS node groups, IRSA, etc.
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:GetRole",
+          "iam:PassRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PutRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "iam:CreatePolicy",
+          "iam:DeletePolicy",
+          "iam:GetPolicy",
+          "iam:GetPolicyVersion",
+          "iam:ListPolicyVersions",
+          "iam:CreatePolicyVersion",
+          "iam:DeletePolicyVersion",
+          "iam:CreateInstanceProfile",
+          "iam:DeleteInstanceProfile",
+          "iam:GetInstanceProfile",
+          "iam:AddRoleToInstanceProfile",
+          "iam:RemoveRoleFromInstanceProfile",
+          "iam:CreateOpenIDConnectProvider",
+          "iam:DeleteOpenIDConnectProvider",
+          "iam:GetOpenIDConnectProvider",
+          "iam:TagOpenIDConnectProvider",
+          "iam:ListOpenIDConnectProviders",
+          "iam:TagRole",
+          "iam:UntagRole",
+          "iam:ListRoleTags",
+          "iam:UpdateAssumeRolePolicy"
+        ]
+        Resource = "*"
+      },
+
+      ############################################
+      # VPC and networking
+      # Needed to create VPC, subnets, NAT, IGW
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*"
+        ]
+        Resource = "*"
+      },
+
+      ############################################
+      # EKS — create and manage cluster
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:*"
+        ]
+        Resource = "*"
+      },
+
+      ############################################
+      # CloudWatch logs — EKS control plane logs
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogDelivery",
+          "logs:DescribeLogGroups",
+          "logs:DeleteLogGroup",
+          "logs:PutRetentionPolicy",
+          "logs:TagLogGroup",
+          "logs:ListTagsLogGroup",
+          "logs:ListTagsForResource",
+          "logs:TagResource"
+        ]
+        Resource = "*"
+      },
+
+      ############################################
+      # KMS — general key management for EKS
+      ############################################
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:CreateKey",
+          "kms:CreateAlias",
+          "kms:DescribeKey",
+          "kms:EnableKeyRotation",
+          "kms:GetKeyPolicy",
+          "kms:GetKeyRotationStatus",
+          "kms:ListAliases",
+          "kms:ListKeys",
+          "kms:PutKeyPolicy",
+          "kms:ScheduleKeyDeletion",
+          "kms:TagResource",
+          "kms:UntagResource"
+        ]
+        Resource = "*"
+      }
+    ]
   })
 }
